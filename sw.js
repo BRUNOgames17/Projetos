7476086f625d3c7098bc06e449a95858a6b3c20e
BRUNOgames17 <74215473+BRUNOgames17@users.noreply.github.com>
2020-12-04 16:43:56 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,14 +5,12 @@ self.addEventListener('install', function(evt){
 
 self.addEventListener('active', function(evt){
   console.log('active sw');
-  evt.waitUntil(caches.keys( ).then(function(keys)){
-return promise.all(
+  evt.waitUntil(caches.keys().then(function(keys){
+return Promise.all(
    keys
   .filter(key => key !== cacheName)
   .map(key => caches.delete(key))
 )}
-)
-
 })
 
 self.addEventListener('fetch', function(evt){
